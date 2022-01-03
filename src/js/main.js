3e2d573386f07ab3d52bc0edc9233ad1af992338
Nikolai Kunakov <nkunakov10@gmail.com>
2022-01-03 21:03:28 +0400

    add "burger-menu"

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 // Custom scripts
+
+//Burger menu
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger__active');
+    menu.classList.toggle('menu__active');
+});
